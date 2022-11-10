c61e4d6da4f529157b9f76a0f91781d35502b19e
VinayKumar1801 <107485549+VinayKumar1801@users.noreply.github.com>
2022-11-10 12:40:38 +0530

    home page

---
+++ front-end/src/pages/home/HomeLower.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  Flex,
+  Image,
+  Button,
+  Input,
+} from "@chakra-ui/react";
+import "./Home.css";
+
+const HomeLower = () => {
+  return (
+    <Box className="lower_side" mt={"5rem"}>
+      <Flex justifyContent={{lg:"right",md:"right",sm:"center",base:"center"}}>
+        <Box bg={"white"} p={"3rem"} boxShadow="md" rounded="md">
+          <Heading
+            size={{ lg: "2xl", md: "xl", sm: "md", base: "md" }}
+            width={{ lg: "80%", md: "80%", base: "100%", sm: "100%" }}
+            m={"8px"}
+          >
+            Healthy Meals Delivered Weekly
+          </Heading>
+          <Input
+            w={"70%"}
+            m={"1rem"}
+            type="text"
+            placeholder="freshly@freshly.com"
+          />
+          <Button
+            color={"white"}
+            backgroundColor={"blue"}
+            p={{ lg: "1.5rem", md: "1.3rem", sm: "1rem", base: "0.7rem" }}
+            m={{ lg: "1.5rem", md: "1.3rem", sm: "1rem", base: "1rem" }}
+            fontSize={"lg"}
+          >
+            Get Started!
+          </Button>
+          <Text>
+            Already have an account?
+            <a href="">Log in.</a>{" "}
+          </Text>
+          <Text>
+            By continuing, you agree to ourTermsandPrivacy Policy.& will receive
+            emails from Freshly.
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default HomeLower;
---
+++ front-end/src/pages/home/Homebottom2.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Box, Heading, Text, Flex, Image, Button } from "@chakra-ui/react";
+import "./Home.css";
+
+const Homebottom2 = () => {
+  return (
+    <Box className="bottom2" mt={"5rem"} mb={"5rem"}>
+     <Flex justifyContent={{lg:"left",md:"left",sm:"center",base:"center"}}>
+        <Box bg={"white"} p={"3rem"} boxShadow="md" rounded="md">
+          <Heading
+            size={{ lg: "2xl", md: "xl", sm: "md", base: "md" }}
+            width={{ lg: "80%", md: "80%", base: "100%", sm: "100%" }}
+            m={"8px"}
+          >
+          We believe eating right should be easy for everyone.
+          </Heading>
+         
+          <Text>
+          Each Freshly meal is perfectly sized for 1 person to enjoy at 1 sitting. Our fully-prepared meals are delivered fresh, and ready to eat in 3 minutes.
+          </Text>
+          <Button
+            color={"white"}
+            backgroundColor={"blue"}
+            p={{ lg: "1.5rem", md: "1.3rem", sm: "1rem", base: "0.7rem" }}
+            m={{ lg: "1.5rem", md: "1.3rem", sm: "1rem", base: "1rem" }}
+            fontSize={"lg"}
+          >
+           Learn More
+          </Button>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Homebottom2;
+
+{
+  /* <Button
+color={"white"}
+backgroundColor={"blue"}
+p={{ lg: "1.5rem", md: "1.3rem", sm: "1rem", base: "0.7rem" }}
+m={{ lg: "1.5rem", md: "1.3rem", sm: "1rem", base: "0rem" }}
+fontSize={"lg"}
+>
+Learn More
+</Button> */
+}
+{
+  /* <Heading
+  m={"8px"}
+  size={{ lg: "2xl", md: "xl", sm: "md", base: "md" }}
+  width={{ lg: "80%", md: "80%", base: "100%", sm: "100%" }}
+>
+  We believe eating right should be easy for everyone.
+</Heading> */
+}
