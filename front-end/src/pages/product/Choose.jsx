81057f9ba25ff696288e1a1b2cdddac187592cde
VinayKumar1801 <107485549+VinayKumar1801@users.noreply.github.com>
2022-11-11 10:48:03 +0530

    data

---
+++ front-end/src/pages/product/Choose.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Flex, Box, Button, Heading, Text } from "@chakra-ui/react";
+import SortDrawer from "./SortDrawer";
+const Choose = () => {
+  return (
+    <Box padding={"1rem"}>
+      <Flex justify={"space-between"} alignContent="center">
+        <Box alignSelf={"center"}>
+          <Text fontWeight={"700"}>Choose Meals</Text>
+        </Box>
+        <Box>
+          <SortDrawer />
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Choose;
