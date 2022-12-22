4b9d4dceb6965f62ea1421fc715b28f8468a7902
arjunsen <arjunsen717@gmail.com>
2022-12-22 14:36:11 +0530

    added admin login loading and toast

--- front-end/src/redux/adminLogin/adminLogin.action.js
+++ front-end/src/redux/adminLogin/adminLogin.action.js
@@ -3,13 +3,16 @@ import {
   ADMIN_LOGIN,
   ADMIN_LOGIN_ERROR,
   ADMIN_LOGOUT,
+  ISLoding
 } from "./adminLogin.type";
 
 const mainUrl = process.env.REACT_APP_MAIN_URL;
 
 export const adminLogin = (cred) => async (dispatch) => {
+  dispatch({type : ISLoding})
   try {
     let res = await axios.post(`${mainUrl}/admin/login`, cred);
+    console.log(res.data)
     dispatch({ type: ADMIN_LOGIN, payload: res.data });
     // console.log(res.data);
     return res.data;
