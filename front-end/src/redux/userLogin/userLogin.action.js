c29f3ff4171ff122d3d8df0ad7ca233541b96dcd
arjunsen <arjunsen717@gmail.com>
2022-12-22 21:12:53 +0530

    fixed login page

--- front-end/src/redux/userLogin/userLogin.action.js
+++ front-end/src/redux/userLogin/userLogin.action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { USER_LOGIN, USER_LOGIN_ERROR, IS_LOADING, USER_LOGOUT } from "./userLogin.type";
+import { USER_LOGIN, USER_LOGIN_ERROR, IS_LOADING, USER_LOGOUT, USER_LODING_FALSE } from "./userLogin.type";
 
 const mainUrl = process.env.REACT_APP_MAIN_URL;
 
@@ -14,6 +14,7 @@ export const userLogin = (cred) => (dispatch) => {
         .catch((err) => {
             
             dispatch({ type: USER_LOGIN_ERROR, payload : err.response.data.msg })
-        })
+        });
+        // dispatch({type : USER_LODING_FALSE})
 };
 export const userLogout = () => ({ type: USER_LOGOUT });
--- front-end/src/redux/userLogin/userLogin.type.js
+++ front-end/src/redux/userLogin/userLogin.type.js
@@ -2,3 +2,4 @@ export const USER_LOGIN = "userLogin/loginSuccess";
 export const USER_LOGOUT = "userLogout/logoutSuccess";
 export const  IS_LOADING = "userisloading/loading"
 export const USER_LOGIN_ERROR = "userLogin/Error";
+export const USER_LODING_FALSE = "USER_LODING_FALSE";
