f82fcc421431be13bd36ca4b6e90d0308417ed07
arjunsen <arjunsen717@gmail.com>
2022-12-23 17:36:24 +0530

    added review features in backend

---
+++ back-end/Models/Review.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+const reviewSchema = mongoose.Schema({
+    productId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "product"
+    },
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "product"
+    },
+    comment : {
+        type : String,
+        required : true
+    }
+},{
+    timestamps : true,
+    versionKey : false
+});
+
+const reviewModel = mongoose.model('review', reviewSchema);
+module.exports = {reviewModel}
